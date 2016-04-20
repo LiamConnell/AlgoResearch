b9699f5ed290b41f972406c7b69e2dcb9a426f51
Liam Connell <liamconnell@Liams-MacBook-Air.local>
2016-04-20 12:19:50 -0300

    all_results started

--- GetBasicData/exec_basic_data.py
+++ GetBasicData/exec_basic_data.py
@@ -24,11 +24,15 @@ datadir_path = ''.join(('../data/', datadir_name))
 if os.path.exists(datadir_path) == False:
     os.makedirs(datadir_path)
     symbolsDF[datadir_name] = False
+    symbolsDF['startdate'] = None
+    symbolsDF['enddate'] = None
     for symbol in symbolsDF.symbols:
         try:
             df = web.DataReader(symbol, 'yahoo', start, end)
             df.to_pickle(''.join((datadir_path, '/', symbol, '.pkl')))
             symbolsDF[datadir_name][symbol] = True
+            symbolsDF['startdate'][symbol] = df.index[0]
+            symbolsDF['enddate'][symbol] = df.index[-1]
         except:
             pass
         
